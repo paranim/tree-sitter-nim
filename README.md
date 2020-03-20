883348e033d5cea07a4e4299ffe10bc26eb64904
oakes <zach@sekao.net>
2020-03-20 14:31:53 -0400

    Update more files to reflect nim instead of python

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-tree-sitter-python
+tree-sitter-nim
 ==================
 
-[![Build Status](https://travis-ci.org/tree-sitter/tree-sitter-python.svg?branch=master)](https://travis-ci.org/tree-sitter/tree-sitter-python)
-[![Build status](https://ci.appveyor.com/api/projects/status/ddxtf154nsck4wbe/branch/master?svg=true)](https://ci.appveyor.com/project/maxbrunsfeld/tree-sitter-python/branch/master)
+Install the tree-sitter CLI tool with:
 
-Python grammar for [tree-sitter][].
+```
+npm install tree-sitter-cli
+```
 
-[tree-sitter]: https://github.com/tree-sitter/tree-sitter
+Then you can re-generate `src/parser.c` with:
 
-#### References
-
-* [Python 2 Grammar](https://docs.python.org/2/reference/grammar.html)
-* [Python 3 Grammar](https://docs.python.org/3/reference/grammar.html)
+```
+tree-sitter generate
+```
--- src/binding.cc
+++ src/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_python();
+extern "C" TSLanguage * tree_sitter_nim();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_python());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_nim());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("python").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("nim").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_python_binding, Init)
+NODE_MODULE(tree_sitter_nim_binding, Init)
 
 }  // namespace
